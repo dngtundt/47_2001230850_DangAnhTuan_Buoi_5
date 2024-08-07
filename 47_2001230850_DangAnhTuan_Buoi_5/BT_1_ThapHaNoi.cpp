41d4062239c1a4c47b35398fba4381358fa5614f
dngtundt <dtun11111@gmail.com>
2024-08-07 14:10:48 +0700

    BT_1_ThapHaNoi finished

---
+++ 47_2001230850_DangAnhTuan_Buoi_5/BT_1_ThapHaNoi.cpp
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void moveDisk(int disk, char fromRod, char toRod) {
+    printf("Chuyển đĩa %d từ cọc %c sang cọc %c\n", disk, fromRod, toRod);
+}
+
+void hanoi(int n, char fromRod, char toRod, char auxRod) {
+    if (n == 1) {
+        moveDisk(n, fromRod, toRod);
+        return;
+    }
+    hanoi(n - 1, fromRod, auxRod, toRod);  // n - 1 A to B
+    moveDisk(n, fromRod, toRod);           // n A to C
+    hanoi(n - 1, auxRod, toRod, fromRod);  // n - 1 B to C
+}
+
+int main() {
+    int n; 
+    printf("Nhập số đĩa: ");
+    scanf("%d", &n);
+
+    hanoi(n, 'A', 'C', 'B');
+
+    return 0;
+}
